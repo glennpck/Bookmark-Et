bfc923af7514a6a7a286da1576c22f90a45fcd9b
glennpck <glennchia43@gmail.com>
2023-10-09 12:25:53 +0800

    debug complete

--- bargain/__init__.py
+++ bargain/__init__.py
@@ -2,3 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9d87de6a296e5fafce4eb599607a9dd4'
+
+from bargain import routes
--- bargain/routes.py
+++ bargain/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect
 from bargain import app
 
-@app.route("/home")
+@app.route("/")
 def index():
-    return render_template('test.html')
+    return render_template('index.html')
