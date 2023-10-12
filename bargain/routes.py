58dd1269178829123028fd037313567ca5307b4b
glennpck <glennchia43@gmail.com>
2023-10-12 09:11:32 +0800

    html touchup + forms

--- bargain/routes.py
+++ bargain/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, url_for, flash, redirect
+from bargain.forms import SearchForm
 from bargain import app
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    form = SearchForm()
+    return render_template('index.html', search=form)
