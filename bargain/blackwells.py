8ab7ab11b4f2b9ff97169a535e91a79992812d9f
glennpck <glennchia43@gmail.com>
2023-10-13 11:59:56 +0800

    scraping blackwells function

--- bargain/blackwells.py
+++ bargain/blackwells.py
@@ -12,7 +12,9 @@ def search(keyword, hits):
     return HTMLParser(r.text)
 
 def bw_scrape(keyword, hits):
+    bw_url = 'https://blackwells.co.uk'
     page = search(keyword, hits)
+    book_list = []
     
     for item in page.css("li.search-result__item"):
 
@@ -24,3 +26,23 @@ def bw_scrape(keyword, hits):
                 title = node.text()
             except KeyError:
                 pass
+
+        type_pbd_nodes = page.css("p.product-format > span")
+        type = type_pbd_nodes[0].text()
+        pbd = type_pbd_nodes[1].text()
+
+        book = Blackwells(
+            isbn = item.css_first("a.btn").attributes['data-isbn'],
+            title = title,
+            desc = "",
+            author = item.css_first("p.product-author").text(),
+            cover = bw_url + str(item.css_first("img").attributes['src']),
+            type = type,
+            pb_date = pbd,
+            price = item.css_first("li.product-price--current").text().strip(),
+            url = bw_url + str(item.css_first("a.product-name").attributes["href"])
+        )
+
+        book_list.append(book)
+
+    return(book_list)
